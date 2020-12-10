033b03d23be4e228ec952f5bba651d0be3cf6742
Margarita <marharyta.obraztsova@gmail.com>
2020-12-10 14:25:25 +0200

    fix camera position to depict moon correctly

--- main.js
+++ main.js
@@ -12,14 +12,12 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-var loader = new _GLTFLoader.GLTFLoader();
-console.log("loader", loader); // Get a reference to the container element that will hold our scene
+// Get a reference to the container element that will hold our scene
+var container = document.querySelector('#scene-container'); // create game loop
+// Initialise
 
-var container = document.querySelector('#scene-container');
 var scene = new THREE.Scene();
 scene.background = new THREE.Color('skyblue'); // Create a camera
-// var cameraZ = 20
-// var perspective = 800
 
 var fov = 90; // AKA Field of View
 // const fov = 180 * ( 2 * Math.atan( window.innerHeight / 2 / perspective ) ) / Math.PI
@@ -27,39 +25,46 @@ var fov = 90; // AKA Field of View
 var aspect = container.clientWidth / container.clientHeight;
 var near = 0.1; // the near clipping plane
 
-var far = 100; // the far clipping plane
+var far = 1000; // the far clipping plane
 
 var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 0, 10); // create the renderer
-
-var renderer = new THREE.WebGLRenderer();
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshStandardMaterial({
-  color: 0x00ff00
-});
-var cube = new THREE.Mesh(geometry, material);
-cube.rotation.set(-0.5, -0.1, 0.8);
+camera.position.set(0, 0, 900);
 var light = new THREE.DirectionalLight('white', 8);
-light.position.set(10, 10, 10);
-var model = null;
-var loadedData = loader.load('Moon_1_3474.glb', function (data) {
-  console.log("big deal", data);
-  scene.add(cube);
-  scene.add(data.scene, light);
-  controls.update();
-  console.log("controls", controls);
-  renderer.render(scene, camera);
-}); // next, set the renderer to the same size as our container element
+light.position.set(10, 10, 10); // create the renderer
+
+var renderer = new THREE.WebGLRenderer(); // next, set the renderer to the same size as our container element
 
 renderer.setSize(container.clientWidth, container.clientHeight); // finally, set the pixel ratio so that our scene will look good on HiDPI displays
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.physicallyCorrectLights = true; // add the automatically created <canvas> element to the page
+renderer.physicallyCorrectLights = true; // create cube 
+
+function cube() {
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshStandardMaterial({
+    color: 0x00ff00
+  });
+  var cube = new THREE.Mesh(geometry, material);
+  cube.rotation.set(-0.5, -0.1, 0.8);
+  return cube;
+} // scene.add(cube(), light);
+
+
+var controls = new _OrbitControls.OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function () {
+  return renderer.render(scene, camera);
+});
+var loader = new _GLTFLoader.GLTFLoader();
+var loadedData = loader.load('Moon_1_3474.glb', function (data) {
+  console.log("big deal", data);
+  var model = data.scene.children[0];
+  console.log('model', model); // model.position.set(0, 0, 90); 
+  // model.scale.set(2, 2, 2);                                 
+
+  scene.add(model, light);
+  renderer.render(scene, camera);
+}); // add the automatically created <canvas> element to the page
 
 container.append(renderer.domElement); // render, or 'create a still image', of the scene
 
-renderer.render(scene, camera);
-var controls = new _OrbitControls.OrbitControls(camera, renderer.domElement);
-controls.update();
-controls.autoRotate = true;
 renderer.render(scene, camera);
--- script.js
+++ script.js
@@ -2,65 +2,62 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const loader = new GLTFLoader();
-console.log("loader", loader)
-
 // Get a reference to the container element that will hold our scene
 const container = document.querySelector('#scene-container');
 
+// create game loop
+// Initialise
 const scene = new THREE.Scene();
-
 scene.background = new THREE.Color('skyblue');
 
 // Create a camera
-// var cameraZ = 20
-// var perspective = 800
 const fov = 90; // AKA Field of View
 // const fov = 180 * ( 2 * Math.atan( window.innerHeight / 2 / perspective ) ) / Math.PI
 const aspect = container.clientWidth / container.clientHeight;
 const near = 0.1; // the near clipping plane
-const far = 100; // the far clipping plane
-
+const far = 1000; // the far clipping plane
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-camera.position.set(0, 0, 10);
-
-// create the renderer
-const renderer = new THREE.WebGLRenderer();
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-
-const cube = new THREE.Mesh( geometry, material );
-cube.rotation.set(-0.5, -0.1, 0.8);
+camera.position.set(0, 0, 900);
 
 const light = new THREE.DirectionalLight('white', 8);
 light.position.set(10, 10, 10);
-let model = null;
-const loadedData = loader.load('Moon_1_3474.glb', 
-                                (data) =>{
-                                    console.log("big deal", data);
-                                    scene.add(cube);
-                                    scene.add(data.scene, light);
-                                    controls.update();
-                                    console.log("controls", controls)
-                                    renderer.render(scene, camera);
-                                    
-                                });
 
+// create the renderer
+const renderer = new THREE.WebGLRenderer();
 // next, set the renderer to the same size as our container element
 renderer.setSize(container.clientWidth, container.clientHeight);
-
 // finally, set the pixel ratio so that our scene will look good on HiDPI displays
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.physicallyCorrectLights = true;
+
+// create cube 
+function cube (){
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.rotation.set(-0.5, -0.1, 0.8);
+    return cube;
+}
+
+// scene.add(cube(), light);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', () => renderer.render(scene, camera));
+
+const loader = new GLTFLoader();
+const loadedData = loader.load('Moon_1_3474.glb', 
+                                 (data) =>{
+                                     console.log("big deal", data); 
+                                     var model = data.scene.children[0];
+                                     console.log('model', model)
+                                     // model.position.set(0, 0, 90); 
+                                     // model.scale.set(2, 2, 2);                                 
+                                     scene.add(model, light);
+                                     renderer.render(scene, camera);                                 
+                                 });
 
 // add the automatically created <canvas> element to the page
 container.append(renderer.domElement);
 
 // render, or 'create a still image', of the scene
 renderer.render(scene, camera);
-
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
-controls.autoRotate = true;
-renderer.render( scene, camera );
